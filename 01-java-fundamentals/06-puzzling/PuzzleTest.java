69e946f21972e88549b4f97e16522674304b8160
Eric Roberts <eric.roberts401@gmail.com>
2021-09-06 13:26:28 -0500

    Updated Puzzling

---
+++ 01-java-fundamentals/06-puzzling/PuzzleTest.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.ArrayList;
+public class PuzzleTest {
+    public static void main(String[] args) {
+        PuzzleJava puzzleTest = new PuzzleJava();
+        //System.out.println(puzzleTest.make10Random());
+        //System.out.println(puzzleTest.charGenerator());
+        //System.out.println(puzzleTest.generatePassword());
+        ArrayList<String> newSet = puzzleTest.getNewPasswordSet(10);
+        for(int i=0; i<newSet.size(); i++){
+            System.out.println(newSet.get(i));
+        }
+    }
+    
+}
