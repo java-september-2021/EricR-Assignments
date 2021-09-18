6313a0012ce079cab6583072bd81f18eacc17114
Eric Roberts <eric.roberts401@gmail.com>
2021-09-18 09:58:48 -0500

    Updated object master pt1

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        Human human2 = new Human();
+
+        human1.attack(human2);
+    }
+}
